a3896013d83b148141355a4f4cff41cab0242d76
preugr <169071364+preugr@users.noreply.github.com>
2024-10-29 11:25:37 +0100

    Correct single user support

--- Core/SimOutage_Blocks.m
+++ Core/SimOutage_Blocks.m
@@ -19,17 +19,28 @@ function [pout] = SimOutage_Blocks(Nsamples,gamma, U, mu, L)
     % Pre-allocating
     pout = zeros(size(gamma));
 
+    % Single user flag
+    singleUser = (U == 1);
+
     % Batch size to avoid RAM issues. Nsamples are generated in batches of
     % size batchsize
     batchsize = 1e3;
 
     % Generate variables by batches
     for batch = 1:batchsize:Nsamples
-        % Generate variables X and Y according to Eq. (26)
-        [Y, X] = GenVariables(batchsize, U, mu, L);
 
-        % Computes the SIR according to Eq. (26)
-        sir = max(X./Y,[], 1);
+        % Single user
+        if singleUser
+            X = GenVariables_SingleUser(batchsize, mu, L);
+            sir = max((1-mu^2)*X,[],1);
+        % Multiuser case
+        else
+            % Generate variables X and Y according to Eq. (26)
+            [Y, X] = GenVariables(batchsize, U, mu, L);
+
+            % Computes the SIR according to Eq. (26)
+            sir = max(X./Y,[], 1);
+        end
 
         % Computes outage probability
         for kg = 1:length(pout)
@@ -37,7 +48,6 @@ function [pout] = SimOutage_Blocks(Nsamples,gamma, U, mu, L)
         end
     end
     pout = pout/Nsamples;
-
 
     %---------------------------------------------------------------------
     %   Auxiliary function that generates correlated samples for the
@@ -72,6 +82,33 @@ function [pout] = SimOutage_Blocks(Nsamples,gamma, U, mu, L)
                     + mu/sqrt(1-mu^2)*randn(1, junk_size,U-1)).^2 + ...
                         (randn(length(b_index),junk_size,U-1)...
                     + mu/sqrt(1-mu^2)*randn(1, junk_size,U-1)).^2, 3);
+            end
+    
+        end
+    
+    end
+
+    function [X] = GenVariables_SingleUser(Nsamples, mu, L)
+    
+        % Number of ports
+        N = sum(L);
+    
+        % Pre-allocating
+        X = zeros(N, Nsamples);
+    
+        % Loop to avoid RAM issues
+        junk_size = 1e3;
+        for ks = 1:ceil(Nsamples/junk_size)
+            junk_index = ((ks-1)*junk_size+1):min(junk_size*ks, Nsamples);
+            % Loop over independent blocks
+            for b = 1:length(L)
+                b_index = sum(L(1:(b-1)))+1:sum(L(1:b));
+                % Correlated samples of channel from/to desired user
+                X(b_index,junk_index) = (randn(length(b_index),junk_size)...
+                    + mu/sqrt(1-mu^2)*randn(1, junk_size)).^2 + ...
+                        (randn(length(b_index),junk_size)...
+                    + mu/sqrt(1-mu^2)*randn(1, junk_size)).^2;
+    
             end
     
         end
